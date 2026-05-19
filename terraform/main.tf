5c22012b8c8f8e563c336951b3836a72275f38f6
Srinivas TP <srinivasnairtp@gmail.com>
2026-05-19 17:15:50 +0000

    Eks module updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,7 @@ module "eks" {
 
   cluster_name    = var.cluster_name
   cluster_version = "1.30"
-
+  cluster_endpoint_public_access = true
   subnet_ids = module.vpc.private_subnets
   vpc_id     = module.vpc.vpc_id
 
